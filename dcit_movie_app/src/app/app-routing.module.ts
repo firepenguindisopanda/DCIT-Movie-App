bd999961e33d0b6921977d78081d36d7f82d151c
Nicholas <nicosmith.smith3@gmail.com>
2021-12-24 17:17:57 -0400

    Added home route

--- dcit_movie_app/src/app/app-routing.module.ts
+++ dcit_movie_app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllMoviesComponent } from './movies/all-movies/all-movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AllMoviesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
