b09335379b083c5f0320497efea50778aa219b38
Nicholas <nicosmith.smith3@gmail.com>
2021-12-24 19:37:35 -0400

    Added style to footer and header

--- dcit_movie_app/src/app/layout/layout.module.ts
+++ dcit_movie_app/src/app/layout/layout.module.ts
@@ -6,6 +6,7 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -19,7 +20,8 @@ import { MatIconModule } from '@angular/material/icon';
     CommonModule,
     MatListModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent,
